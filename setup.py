4d2e8a97f6d8e27d9aa6d9fdd37a4ff44bc334f5
Paul Saxe <paul@thesaxes.net>
2019-08-31 17:49:50 -0400

    Switched to versioneer for versioning.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
-
+"""
+seamm_widgets
+Custom widgets for the SEAMM environment.
+"""
+import sys
 from setuptools import setup, find_packages
+import versioneer
+
+short_description = __doc__.split("\n")
+
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -12,49 +22,60 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'seamm_util>=0.1'
-    # TODO: put any other package requirements here
-]
-
-setup_requirements = [
-    'pytest-runner',
-    # TODO(paulsaxe): put setup requirements (distutils extensions, etc.) here
-]
-
-test_requirements = [
-    'pytest',
-    # TODO: put package test requirements here
+    'seamm-util>=0.5',
 ]
 
 setup(
+    # Descriptive entries which should always be present
     name='seamm_widgets',
-    version='0.2.1',
-    description="Tk widgets to support the plugins in SEAMM",
-    long_description=readme + '\n\n' + history,
     author="Paul Saxe",
-    author_email='psaxe@vt.edu',
-    url='https://github.com/molssi-seamm/seamm_widgets',
+    author_email='psaxe@molssi.org',
+    description=short_description[1],
+    long_description=readme + '\n\n' + history,
+    version=versioneer.get_version(),
+    # version='0.1.0',
+    cmdclass=versioneer.get_cmdclass(),
+    license='BSD-3-Clause',
+    url='https://github.com/molssi-seam/seamm_widgets',
+
+    # Which Python importable modules should be included when your package is
+    # installed, handled automatically by setuptools. Use 'exclude' to prevent
+    # some specific subpackage(s) from being added, if needed
     packages=find_packages(include=['seamm_widgets']),
+
+    # Optional include package data to ship with your package. Customize
+    # MANIFEST.in if the general case does not suit your needs. Comment out
+    # this line to prevent the files from being packaged with your software
     include_package_data=True,
+
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+
+    # Required packages, pulls from pip if needed; do not use for Conda
+    # deployment
     install_requires=requirements,
-    license="BSD license",
-    zip_safe=False,
+
+    test_suite='tests',
+
+    # Valid platforms your code works on, adjust to your flavor
+    platforms=['Linux',
+               'Mac OS-X',
+               'Unix',
+               'Windows'],
+
+    # Manual control if final package is compressible or not, set False to
+    # prevent the .egg from being made
+    # zip_safe=False,
+
     keywords='seamm_widgets',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
     ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
-    entry_points={
-    }
 )
