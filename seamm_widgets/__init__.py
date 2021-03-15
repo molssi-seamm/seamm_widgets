d6c1b6b61b0a1a35e8744f60c6943c7fba680c70
Paul Saxe <paul@thesaxes.net>
2021-03-15 02:23:52 -0400

    Added code to get versions from Versioneer.

--- seamm_widgets/__init__.py
+++ seamm_widgets/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for SEAMM Widgets."""
-
-__author__ = """Paul Saxe"""
-__email__ = 'psaxe@vt.edu'
-__version__ = '0.2.1'
+"""
+seamm_widgets
+Custom Tk widgets for SEAMM.
+"""
 
 # Bring up the classes so that they appear to be directly in
 # the seamm_widgets package.
@@ -20,6 +19,16 @@ from seamm_widgets.labeled_combobox import LabeledCombobox  # noqa: F401
 from seamm_widgets.periodic_table import PeriodicTable  # noqa: F401
 from seamm_widgets.unit_entry import UnitEntry  # noqa: F401
 from seamm_widgets.unit_combobox import UnitCombobox  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+
+__author__ = """Paul Saxe"""
+__email__ = "psaxe@molssi.org"
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
 
 default_units = {
     'dimensionless': ['degree'],
