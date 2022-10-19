a1fde197ea3de88918425ca9ffc5ffa63df29239
Paul Saxe <paul@thesaxes.net>
2022-10-19 11:19:19 -0400

    Moved default units to seamm_util.

--- seamm_widgets/__init__.py
+++ seamm_widgets/__init__.py
@@ -34,28 +34,3 @@ versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
-
-default_units = {
-    "dimensionless": ["degree"],
-    "[length]": ["Å", "pm", "nm", "um"],
-    "[length] ** 2": ["Å**2", "pm**2", "nm**2", "um**2"],
-    "[length] ** 3": ["Å**3", "pm**3", "nm**3", "um**3"],
-    "1 / [length]": ["1/Å", "1/pm", "1/nm", "1/um"],
-    "[mass]": ["g", "kg", "tonne", "lb", "ton"],
-    "[substance]": ["mol"],
-    "[temperature]": ["K", "degC", "degF", "degR"],
-    "[mass] / [length] ** 3": ["g/mL", "kg/L", "kg/m**3", "g/mol/Å**3"],  # nopep8
-    "[time]": ["fs", "ps", "ns", "us", "ms", "s"],
-    "[length] ** 2 * [mass] / [substance] / [time] ** 2": [
-        "kcal/mol",
-        "kJ/mol",
-    ],  # nopep8
-    "[length] * [mass] / [substance] / [time] ** 2": [
-        "kcal/mol/Å",
-        "kJ/mol/Å",
-        "eV/Å",
-    ],  # nopep8
-    "[length] * [mass] / [time] ** 2": ["kcal/mol/Å", "kJ/mol/Å", "eV/Å"],  # nopep8
-    "[length] / [time] ** 2": ["m/s**2", "ft/s**2", "Å/fs**2"],
-    "[mass] / [length] / [time] ** 2": ["Pa", "atm", "bar", "psi", "ksi"],
-}
